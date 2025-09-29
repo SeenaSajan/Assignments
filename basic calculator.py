7fa6f46e187df798003fd5a68f4f4440c5b0af0b
SeenaSajan <cna.seenarahim@gmail.com>
2025-09-29 18:56:57 +0300

    Add files via upload

---
+++ basic calculator.py
@@ -0,0 +1,27 @@
+num1=float(input('enter the first number: '))
+num2=float(input('enter the second number: '))
+print("1.Addition")
+print("2.subtraction")
+print("3.Multiplication")
+print("4.Division")
+print("5.Modulus")
+print("6.floor")
+print("7.Exponention")
+operation=input('which operation do you want to perform: ')
+if operation == "1" :
+    result=num1+num2
+elif operation=="2":
+    result=num1-num2
+elif operation=="3":
+    result=num1*num2
+elif operation=="4":
+    result=num1/num2
+elif operation=="5":
+    result=num1%num2
+elif operation=="6":
+    result=num1//num2
+elif operation=="7":
+    result=num1**num2
+else:
+     result=print('wrong selection')
+print(result)
