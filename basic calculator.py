a91c30513c8df9a19ce67bb1cd0b0d7674ef60e9
SeenaSajan <cna.seenarahim@gmail.com>
2025-09-29 22:59:17 +0300

    Add files via upload

--- basic calculator.py
+++ basic calculator.py
@@ -25,3 +25,11 @@ elif operation=="7":
 else:
      result=print('wrong selection')
 print(result)
+
+
+
+#task 2
+for i in range (1,6):
+    for j in range(i,0,-1):
+        print(j, end=' ')
+    print()
